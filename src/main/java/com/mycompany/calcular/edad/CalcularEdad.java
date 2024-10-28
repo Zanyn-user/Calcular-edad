6aab83794d2d7edf10a0b043a7ba944760bee43b
Zanyn-user <zanynuser@gmail.com>
2024-10-28 12:19:15 -0500

    cualquier cosa

---
+++ src/main/java/com/mycompany/calcular/edad/CalcularEdad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcular.edad;
+
+/**
+ *
+ * @author User
+ */
+public class CalcularEdad {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
